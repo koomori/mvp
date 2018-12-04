9bad83892c412de7ddab58f5471ac5f3f114fece
Barbara Patten <Barbara.Patten@stearnsbank.com>
2018-12-04 13:03:53 -0600

    initail commit

---
+++ app-admin/grails-app/conf/spring/resources.groovy
@@ -0,0 +1,7 @@
+import app.admin.security.UserPasswordEncoderListener
+import app.admin.security.UserPasswordEncoderListener
+// Place your Spring DSL code here
+beans = {
+    userPasswordEncoderListener(UserPasswordEncoderListener)
+    userPasswordEncoderListener(UserPasswordEncoderListener)
+}
---
+++ app-web/server/grails-app/controllers/app/web/ApplicationController.groovy
@@ -0,0 +1,14 @@
+package app.web
+
+import grails.core.GrailsApplication
+import grails.plugins.*
+
+class ApplicationController implements PluginManagerAware {
+
+    GrailsApplication grailsApplication
+    GrailsPluginManager pluginManager
+
+    def index() {
+        [grailsApplication: grailsApplication, pluginManager: pluginManager]
+    }
+}
